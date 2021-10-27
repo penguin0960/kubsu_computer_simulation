b30ddfccb2c732b53fc57ddc5d8caf0de21b076b
Roman Posevin <posevin.2011@mail.ru>
2021-10-27 20:25:10 +0300

    extended analysis file

--- analysis.py
+++ analysis.py
@@ -2,10 +2,34 @@ from typing import Dict, Union
 
 from data import WEIGHTS
 
+SUCCESS_INTERVIEW_RATING = 0
+YES_NO_VALUES = {
+    'Да': 1,
+    'Нет': 0,
+}
 
-def is_success(user_data: Dict[str: Union[str, int]]):
-    result = 0
-    for key, value in user_data.items():
-        result += value * WEIGHTS[key]
+
+def is_success_interview(answers: Dict[str: str]) -> bool:
+    return calculate_rating(answers) > SUCCESS_INTERVIEW_RATING
+
+
+def calculate_rating(answers: Dict[str: str]) -> int:
+    important_answers = {
+        key: value
+        for key, value in answers.items
+        if key in WEIGHTS
+    }
+    formatted_answers = format_answers(important_answers)
+    rating = 0
+    for key, value in formatted_answers.items():
+        rating += value * WEIGHTS[key]
+
+    return rating
+
+
+def format_answers(answers: Dict[str: str]) -> Dict[str: int]:
+    result = {}
+    for key, value in answers.items():
+        result = YES_NO_VALUES[value] if value in YES_NO_VALUES else int(value)
 
     return result
